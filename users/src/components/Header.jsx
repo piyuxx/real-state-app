e8b3c4e0a5575f93b462752c69101ae373c5842a
piyuxx <piyushsharma3rules@gmail.com>
2024-04-16 19:35:49 +0530

    frontend and backend setup

--- users/src/components/Header.jsx
+++ users/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
