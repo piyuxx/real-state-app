29e9de76d4043bb1086820e2532d2043e49a15a1
piyuxx <piyushsharma3rules@gmail.com>
2024-04-16 21:17:44 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONG0_URI)
     .then(() => {
@@ -10,6 +11,13 @@ mongoose.connect(process.env.MONG0_URI)
         console.log(err)
     })
 const app = express();
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log('Server is running on port 3000s')
 })
+
+app.use('/api/user', userRouter);
+// app.get('/test', (req, res) => {
+//     res.json({
+//         message: "Hello World",
+//     })
+// })
