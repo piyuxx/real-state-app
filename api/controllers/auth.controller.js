57ddb8036843b407d4d3f1ccad4e57cd57a0ea1d
piyuxx <piyushsharma3rules@gmail.com>
2024-04-17 07:20:32 +0530

    add middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,8 +8,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json('User created successfully')
     } catch (error) {
-        res.status(500).json(error.message)
-        console.log(error.message)
+        next(error)
     }
 
 }
