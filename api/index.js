933b81b2e955c0253ccd0aa9400ba4c00d0dac4f
piyuxx <piyushsharma3rules@gmail.com>
2024-04-17 07:03:14 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import User from './models/user.model.js';
+import bodyParser from 'body-parser'; // Import bodyParser
+
 dotenv.config();
 mongoose.connect(process.env.MONG0_URI)
     .then(() => {
@@ -11,13 +15,14 @@ mongoose.connect(process.env.MONG0_URI)
         console.log(err)
     })
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+
 app.listen(5000, () => {
     console.log('Server is running on port 3000s')
 })
 
 app.use('/api/user', userRouter);
-// app.get('/test', (req, res) => {
-//     res.json({
-//         message: "Hello World",
-//     })
-// })
+
+app.use('/api/auth', authRouter);
