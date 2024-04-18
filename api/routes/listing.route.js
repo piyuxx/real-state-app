2df55b79a6348eedf964785d60777664774f8a60
piyuxx <piyushsharma3rules@gmail.com>
2024-04-18 20:06:08 +0530

    changes

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
 const router = express.Router();
 import { verifyToken } from '../utils/verifyUser.js'
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing);
 export default router;
