bb9b41f8b4814c3911faf79a39b78112f4313818
piyuxx <piyushsharma3rules@gmail.com>
2024-04-20 02:05:18 +0530

    add build functionality

--- users/src/index.js
+++ users/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { persister, store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persister={persister} >
+    <PersistGate loading={null} persistor={persistor} >
       <App />
     </PersistGate>
   </Provider>
