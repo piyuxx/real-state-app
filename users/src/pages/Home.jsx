fdd2b44a4b96eb669cc6ae35c76d93f965e0f52f
piyuxx <piyushsharma3rules@gmail.com>
2024-04-20 01:08:29 +0530

    completed

--- users/src/pages/Home.jsx
+++ users/src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+    <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+        Find your next <span className='text-slate-500'>perfect</span> 
+        <br />
+        place with ease
+      </h1>
+      <div className='text-gray-400 text-xs sm:text-sm'>
+         This is the best place to find a property.
+         <br />
+         We have a wide range of properties for you to choose from.
+      </div>
+      <div className='flex gap-6'>
+      <Link to={'/search'} className='text-xs sm:text-sm
+      text-blue-800 font-bold hover:underline'>Search Property...</Link>
+        <Link to='/create-listing' className='text-xs sm:text-sm
+      text-blue-800 font-bold hover:underline'>
+           List your property...
+          </Link>
+          </div>
+    </div>
   )
 }
 
