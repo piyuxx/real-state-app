4c95852c0f08ab00b038bfdf4a31a20c8be1f40a
piyuxx <piyushsharma3rules@gmail.com>
2024-04-20 12:34:16 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -33,11 +33,12 @@ app.use('/api/user', userRouter);
 
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter)
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
+// Handle all other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
