83d3b3d18af715bf6676d436a62f0b98f10fba66
piyuxx <piyushsharma3rules@gmail.com>
2024-04-20 11:15:00 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter)
 app.use(express.static(path.join(__dirname), '/users/build'))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public'))
+    res.sendFile(path.join(__dirname, 'users', 'build', 'public'))
 })
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
